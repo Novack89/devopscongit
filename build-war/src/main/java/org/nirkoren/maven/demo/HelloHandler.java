11f12fa84c84054ea52d1125ede62dfaba820fe8
anovack <anovack@moog.com>
2024-10-07 14:51:26 -0400

    update font family and color. update java class output

--- build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.nirkoren.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Hi, is everything working from your Laptop?";
+		String hello = "Sample output from java class";
 		return hello;
 	}
 
